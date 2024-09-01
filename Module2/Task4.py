91d60c5aaa64d77da9505de507b659454532cedb
Chathurika <silvachathurtika@outlook.com>
2024-09-01 22:32:51 +0300

    Task5 commit

---
+++ Module2/Task4.py
@@ -0,0 +1,7 @@
+
+
+#Exercise 04
+num_1 = int(input("Enter three integer numbers: "))
+num_2 = int(input())
+num_3 = int(input())
+print(f"sum of 3 numbers is{num1+num2+num3},product{num1*num2*num3},average{(num1+num2+num3)/3}")()
