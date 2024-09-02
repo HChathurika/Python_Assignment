09ce2b38f0a24ef094b65ba4cfe1cea74fd5c6ab
HChathurika <160768137+HChathurika@users.noreply.github.com>
2024-09-02 11:02:26 +0300

    Delete Module2/Task3.py

--- Module2/Task3.py
+++
@@ -1,7 +0,0 @@
-#Exercise 03
-lenght = float(input("What is the length of the rectangle:"))
-width = float(input("What is the width of the rectangle:"))
-perimeter = 2*(lenght+width)
-area = lenght*width
-print(f"The perimeter of the rectangle is :", {perimeter})
-print(f"The area of the rectangle is: {arae}")
