2dffd765a2f7fdbe264de3aa516af5ebfce78fd5
Chathurika <silvachathurtika@outlook.com>
2024-09-03 11:19:20 +0300

    mod2task3

--- Module2/Task1.py
+++ Module2/Task1.py
@@ -3,11 +3,7 @@
 name = input("What is your name?")
 print("Hello," + name + "!Nice to meet you")
 
-#Exercise 02
-import math
-radius=float(input("What is the radius of the circle?"))
-area=math.pi*radius**2
-print(f"The area of the circle is for", str(radius),"is:",area)
+
 8
 
 
---
+++ Module2/task2.py
@@ -0,0 +1,5 @@
+#Exercise 02
+import math
+radius=float(input("What is the radius of the circle?"))
+area=math.pi*radius**2
+print(f"The area of the circle is for", str(radius),"is:",area)
