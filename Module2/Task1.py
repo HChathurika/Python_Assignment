913e03d25e25abf8d6a1701c6342dc36cf914ab9
Chathurika <silvachathurtika@outlook.com>
2024-09-03 11:24:51 +0300

    mod2task5

--- Module2/Task1.py
+++ Module2/Task1.py
@@ -14,22 +14,6 @@ print("Hello," + name + "!Nice to meet you")
 
 
 
-#Exercise 04
-num_1 = int(input("Enter first numbers: "))
-num_2 = int(input("Enter second number: "))
-num_3 = int(input("Enter third number: "))
-
-#Calculate sum of three numbers
-sum=num_1+num_2+num_3
-print("Sum of Three numbers:",sum)
-
-#calculate product of three numbers
-product=num_1*num_2*num_3
-print("Product of Three numbers:",product)
-
-#calculate average of three numbers
-average=sum/3
-print("Average of Three numbers:",average)
 
 #Exercise 05
 talents = float (input("Enter talents:"))
---
+++ Module2/Task4.py
@@ -0,0 +1,16 @@
+#Exercise 04
+num_1 = int(input("Enter first numbers: "))
+num_2 = int(input("Enter second number: "))
+num_3 = int(input("Enter third number: "))
+
+#Calculate sum of three numbers
+sum=num_1+num_2+num_3
+print("Sum of Three numbers:",sum)
+
+#calculate product of three numbers
+product=num_1*num_2*num_3
+print("Product of Three numbers:",product)
+
+#calculate average of three numbers
+average=sum/3
+print("Average of Three numbers:",average)
