a494c15c3c09f9184970f30c1f90f5f0a1347cb5
Chathurika <silvachathurtika@outlook.com>
2024-09-07 08:51:18 +0300

    done5

---
+++ Module6/Task4.py
@@ -0,0 +1,6 @@
+def sum_all_nums_list(numbers):
+    return sum(numbers)
+
+list = [2, 5, 9, 15, 8,]
+total = sum_all_nums_list(list)
+print("The sum of all numbers I enterd in the list is :",total)
