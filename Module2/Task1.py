16cd88d41f79d242717072f6e127ddd7b62135f1
Chathurika <silvachathurtika@outlook.com>
2024-09-14 18:59:33 +0300

    updat

--- Module2/Task1.py
+++ Module2/Task1.py
@@ -5,31 +5,12 @@ print("Hello," + name + "!Nice to meet you")
 
 
 
-__ == "__main__":
 
 
 
 
 
 
-
-
-
-
-
-    gender = input("Enter your gender: ")
-    hemoglobin = input("Enter your  value: ")
-
-    if gender == "feMale":
-
-        if divisible_by_four and divisible_by_400:
-            print(f"{year} is not a leap year")
-        else:
-            print(f"{year} is not a leap year")
-
-
-
-name
 
 
 
---
+++ Module4/Task1.py
@@ -0,0 +1,13 @@
+num = 1
+
+#take numbers 1 to 1000
+
+while num <= 1000:
+    # Check the number is divisible by 3
+    if num % 3 == 0:
+        print(num)
+
+
+
+    # If number doesn't divisible by 3 go to the nother
+    num += 1
